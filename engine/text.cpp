49c1222f5555d6e4f4abe1358dec791235632283
Ho1ocron <cmrfreeze@gmail.com>
2026-01-03 16:20:37 +0500

    [+] added struct text to render it faster

---
+++ engine/text.cpp
@@ -0,0 +1,137 @@
+#include "text.h"
+
+
+Text::Text(
+    const char* text, const char* fontPath,
+    glm::vec3 position,float scale,
+    const unsigned int& screen_width,
+    const unsigned int& screen_height,
+    glm::vec3 color
+) : content(text),
+    position(position),
+    scale(scale),
+    screen_height(screen_height),
+    screen_width(screen_width),
+    color(color),
+    shader(SHADER_DIR "text.vs", SHADER_DIR "text.fs")
+{
+    loadFont(fontPath);
+    setupBuffers();
+
+    glm::mat4 projection = glm::ortho(0.0f, static_cast<float>(screen_width), 0.0f, static_cast<float>(screen_height));
+    shader.use();
+    shader.setMat4("projection", projection);
+}
+
+
+void Text::loadFont(const std::string& path, unsigned int size) {
+    FT_Library ft;
+    FT_Init_FreeType(&ft);
+
+    FT_Face face;
+    FT_New_Face(ft, path.c_str(), 0, &face);
+    FT_Set_Pixel_Sizes(face, 0, size);
+
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+
+    for (unsigned char c = 0; c < 128; c++) {
+        FT_Load_Char(face, c, FT_LOAD_RENDER);
+
+        unsigned int texture;
+        glGenTextures(1, &texture);
+        glBindTexture(GL_TEXTURE_2D, texture);
+
+        glTexImage2D(
+            GL_TEXTURE_2D,
+            0,
+            GL_RED,
+            face->glyph->bitmap.width,
+            face->glyph->bitmap.rows,
+            0,
+            GL_RED,
+            GL_UNSIGNED_BYTE,
+            face->glyph->bitmap.buffer
+        );
+
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+        font.characters[static_cast<char>(c)] = {
+            texture,
+            { face->glyph->bitmap.width, face->glyph->bitmap.rows },
+            { face->glyph->bitmap_left, face->glyph->bitmap_top },
+            static_cast<unsigned int>(face->glyph->advance.x)
+        };
+    }
+
+    FT_Done_Face(face);
+    FT_Done_FreeType(ft);
+}
+
+
+void Text::setupBuffers() {
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 6 * 4, nullptr, GL_DYNAMIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(float), 0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+}
+
+
+void Text::setScreenSize(const unsigned int& width, const unsigned int& height) {
+    glm::mat4 projection = glm::ortho(0.0f, static_cast<float>(width), 0.0f, static_cast<float>(height));
+    shader.use();
+    shader.setMat4("projection", projection);
+}
+
+
+void Text::render() {
+    shader.use();
+    shader.setVec3("textColor", color);
+
+    glActiveTexture(GL_TEXTURE0);
+    glBindVertexArray(VAO);
+
+    float x = position.x;
+    float y = position.y;
+
+    for (char c : content) {
+        const Character& ch = font.characters.at(c);
+
+        float xpos = x + ch.Bearing.x * scale;
+        float ypos = y - (ch.Size.y - ch.Bearing.y) * scale;
+
+        float w = ch.Size.x * scale;
+        float h = ch.Size.y * scale;
+
+        float vertices[6][4] = {
+            { xpos,     ypos + h, 0.0f, 0.0f },
+            { xpos,     ypos,     0.0f, 1.0f },
+            { xpos + w, ypos,     1.0f, 1.0f },
+
+            { xpos,     ypos + h, 0.0f, 0.0f },
+            { xpos + w, ypos,     1.0f, 1.0f },
+            { xpos + w, ypos + h, 1.0f, 0.0f }
+        };
+
+        glBindTexture(GL_TEXTURE_2D, ch.TextureID);
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices);
+
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+
+        x += (ch.Advance >> 6) * scale;
+    }
+
+    glBindVertexArray(0);
+    glBindTexture(GL_TEXTURE_2D, 0);
+}
---
+++ engine/text.h
@@ -0,0 +1,60 @@
+#pragma once
+
+#include <map>
+#include <string>
+
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <ft2build.h>
+#include FT_FREETYPE_H
+
+#include "shader.h"
+
+
+struct Character {
+    unsigned int TextureID;
+    glm::ivec2 Size;
+    glm::ivec2 Bearing;
+    unsigned int Advance;
+};
+
+
+struct Font {
+    std::map<char, Character> characters;
+};
+
+
+struct Text 
+{
+public:
+    Text(
+        const char* text, const char* fontPath,
+        glm::vec3 position, float scale, 
+        const unsigned int& screen_width,
+        const unsigned int& screen_height,
+        glm::vec3 color
+    );
+
+    void render();
+    void setScreenSize(const unsigned int& width, const unsigned int& height);
+
+private:
+    void loadFont(const std::string& path, unsigned int size = 24);
+    void setupBuffers();
+
+    const unsigned short int screen_width;
+    const unsigned short int screen_height;
+    std::string content;
+    glm::vec2 position;
+    float scale;
+    glm::vec3 color;
+
+    Font font;
+    Shader shader;
+
+    unsigned int VBO{};
+    unsigned int VAO{};
+};
